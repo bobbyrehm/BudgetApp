218c43a9221d1d4f586296890412bddefa989ad5
Bobby Rehm <bobbyrehm502@gmail.com>
2019-07-27 20:05:19 -0700

    Initial Commit

---
+++ Budget/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Budget
+//
+//  Created by Bobby Rehm on 7/27/19.
+//  Copyright © 2019 Bobby Rehm. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
